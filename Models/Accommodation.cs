05da4dd848008b7c7ead20b2654dc29cd0e50809
davidooo10 <joshe1994@yahoo.com>
2025-11-13 17:37:06 -0700

    Added Validation to destination and start and end date.

--- Models/Accommodation.cs
+++ Models/Accommodation.cs
@@ -1,9 +1,13 @@
-﻿namespace TripsLog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TripsLog.Models
 {
     public class Accommodation
     {
         public int AccommodationId { get; set; }
-        public string Name { get; set; }
+
+        [Display(Name = "Accommodation")]
+        public string? Name { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
 
--- Models/Trip.cs
+++ Models/Trip.cs
@@ -1,27 +1,46 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TripsLog.Models
 {
-    public class Trip
+    public class Trip : IValidatableObject
     {
         public int TripId { get; set; }
+
+        [Required(ErrorMessage = "Destination is required.")]
         public string Destination { get; set; }
 
-
-        [DisplayFormat(DataFormatString = "{MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Start Date is required.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime StartDate { get; set; }
 
-        [DisplayFormat(DataFormatString = "{MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "End Date is required.")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EndDate { get; set; }
 
         public int AccommodationId { get; set; }
 
-        public Accommodation Accommodation { get; set; } = new Accommodation();
+        // Accommodation is now optional
+        public Accommodation? Accommodation { get; set; } = new Accommodation();
+
 
         public int TodoId { get; set; }
 
         public List<Todo> Todos { get; set; } = new List<Todo>();
 
-
+        // Custom validation to ensure EndDate is not before StartDate
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (EndDate < StartDate)
+            {
+                yield return new ValidationResult(
+                    "End Date cannot be earlier than Start Date.",
+                    new[] { nameof(EndDate) }
+                );
+            }
+        }
     }
 }
